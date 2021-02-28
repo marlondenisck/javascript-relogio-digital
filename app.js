4a3bbc3ccf29869c58cf8a78579252e01267ecd2
marlondck <15050029+marlondck@users.noreply.github.com>
2021-02-27 20:44:33 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const clockContainer = document.querySelector('.clock-container')
 
+const formatTimeUnit = unit => `${String(unit).length === 1 ? `0${unit}` : unit}`
+
+const clockContainer = document.querySelector('.clock-container')
+
+const getClockHTML = (hours, minutes, seconds) => `
+  <span>${formatTimeUnit(hours)}</span> :
+  <span>${formatTimeUnit(minutes)}</span> :
+  <span>${formatTimeUnit(seconds)}</span>
+`
+
 const updateClock = () => {
   const present = new Date()
   const hours = present.getHours()
   const minutes = present.getMinutes()
   const seconds = present.getSeconds()
 
-  const clockHTML = `
-    <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
-    <span>${String(minutes).length === 1 ? `0${minutes}` : minutes }</span> :
-    <span>${String(seconds).length === 1 ? `0${seconds}` : seconds }</span>
-  `
 
-  clockContainer.innerHTML =  clockHTML
+  clockContainer.innerHTML = getClockHTML(hours, minutes, seconds)
 }
 
 setInterval(updateClock, 1000)
